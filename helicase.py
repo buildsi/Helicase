b40d1bef344411b839c9d731ae1ede0299a38620
Alec Scott <alec@bcs.sh>
2021-06-29 10:53:44 -0700

    Add Print Debug Possibility

--- helicase.py
+++ helicase.py
@@ -6,11 +6,19 @@ class Helicase:
     def analyze(self, commit):
         return
 
-    def traverse(self, path:str, since:datetime=None, to:datetime=None, checkout:bool=False):
+    def traverse(self, path:str, since:datetime=None, to:datetime=None, checkout:bool=False, printTrial:bool=False):
         repo = Repo(path)
 
+        if printTrial:
+            count = 0
+            for commit in Repository(path, since=since, to=to).traverse_commits():
+                count += 1
+            i = 0
         for commit in Repository(path, since=since, to=to).traverse_commits():
             if checkout:
                 repo.git.checkout(commit.hash)
+            if printTrial:
+                i += 1
+                print(f"Analyzing Commit [{i}/{count}]")
             self.analyze(commit)
     
