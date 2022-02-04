ba9603d88ace80c5b66001fac26166b73b5c4f56
Alec Scott <hi@alecbcs.com>
2022-02-04 13:08:00 -0700

    Test limiting commits to only those within the main branch

--- helicase.py
+++ helicase.py
@@ -15,6 +15,11 @@ class Helicase:
                 count += 1
             i = 0
         for commit in Repository(path, since=since, from_commit=from_commit, to=to, to_commit=to_commit, only_in_branch=repo.active_branch.name, only_no_merge=True).traverse_commits():
+            # If the commit exists in more branches than just the main branch that very
+            # likely means that it was committed in a separate branch and merged in later
+            # which is not what we want to analyze.
+            if repo.active_branch.name not in commit.branches or len(commit.branches) > 1:
+                continue
             if checkout:
                 repo.git.checkout(commit.hash)
             if printTrial:
